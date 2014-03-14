c5474044141919624e44de5e39e9619b462b8ad3
Alex Reed (Xerax) <alexreed@outlook.com>
2014-03-14 14:11:46 +0000

    Using cleanup, documentation, and added a function to be able to update the authentication

--- Solution/ImgurNet/Imgur.cs
+++ Solution/ImgurNet/Imgur.cs
@@ -1,20 +1,33 @@
 ﻿using System;
-using ImgurNet.ApiEndpoints;
 using ImgurNet.Authentication;
-using ImgurNet.Models;
 
 namespace ImgurNet
 {
 	public class Imgur
 	{
+		/// <summary>
+		/// The authentication that this Client uses
+		/// </summary>
 		public IAuthentication Authentication { get; private set; }
 		
 		/// <summary>
-		/// 
+		/// Creates a new instance of the Imgur Client, that can be passed into Endpoints
 		/// </summary>
-		/// <param name="authentication"></param>
+		/// <param name="authentication">Your authentication.</param>
 		public Imgur(IAuthentication authentication)
 		{
+			Authentication = authentication;
+		}
+
+		/// <summary>
+		/// Updates the authentication that this Client uses.
+		/// </summary>
+		/// <param name="authentication">The authentication to put into this Client.</param>
+		public void ChangeAuthentication(IAuthentication authentication)
+		{
+			if (authentication.AuthenticationType == AuthenticationType.OAuth)
+				throw new NotImplementedException();
+
 			Authentication = authentication;
 		}
 	}
